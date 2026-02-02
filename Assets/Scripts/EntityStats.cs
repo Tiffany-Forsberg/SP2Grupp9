6ee1bb837a301d8cd3bd7674fa28baec84bca5e8
Tiffany Forsberg <tiff.forsberg@gmail.com>
2026-02-02 13:45:41 +0100

    Fixed entity stats
    Is now simple again :)

--- Assets/Scripts/EntityStats.cs
+++ Assets/Scripts/EntityStats.cs
@@ -1,23 +1,24 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EntityStats : MonoBehaviour
 {
     [field: Header("Damage and Attacks")]
-    [field: SerializeField] public float DamageMultiplier { get; private set; } = 1.0f;
-    [field: SerializeField] public int FlatDamageIncrease { get; private set; } = 0;
-    [field: SerializeField] public float KnockbackMultiplier { get; private set; } = 1.0f;
-    [field: SerializeField] public float AttackSpeedMultiplier { get; private set; } = 1.0f;
+    public float DamageMultiplier = 1.0f;
+    public int FlatDamageIncrease = 0;
+    public float KnockbackMultiplier = 1.0f;
+    public float AttackSpeedMultiplier = 1.0f;
 
     [field: Header("Health")]
-    [field: SerializeField] public float HealthMultiplier { get; private set; } = 1.0f;
-    [field: SerializeField] public int FlatHealthIncrease { get; private set; } = 0;    
+    public float HealthMultiplier = 1.0f;
+    public int FlatHealthIncrease = 0;    
 
     [field: Header("Speed and Movement")]
-    [field: SerializeField] public float SpeedMultiplier { get; private set; } = 1.0f;
-    [field: SerializeField] public float FlatSpeedIncrease { get; private set; } = 0;
-    [field: SerializeField] public float AccelerationMultiplier { get; private set; } = 1.0f;
-    [field: SerializeField] public float DecelerationMultiplier { get; private set; } = 1.0f;
+    public float SpeedMultiplier = 1.0f;
+    public float FlatSpeedIncrease = 0;
+    public float AccelerationMultiplier = 1.0f;
+    public float DecelerationMultiplier = 1.0f;
     
     [field: Header("Miscellaneous")]
-    [field: SerializeField] [Min(0)] public float CooldownMultiplier { get; private set; } = 1.0f;
+    [Min(0)] public float CooldownMultiplier = 1.0f;
 }
