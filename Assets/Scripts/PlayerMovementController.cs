3ba9d2d30d9bb5fe1b1887927f792365209334af
Tiffany Forsberg <tiff.forsberg@gmail.com>
2026-02-03 11:34:26 +0100

    Add jump input action
    Co-Authored-By: a24phizi <187495987+a24phizi@users.noreply.github.com>

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -5,6 +5,8 @@ using UnityEngine.InputSystem;
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] private EntityMovement entityMovement;
+    
+    [SerializeField] private GroundCheck groundCheck;
 
     private int _heldMovementInput;
 
@@ -18,5 +20,14 @@ public class PlayerMovementController : MonoBehaviour
         Vector2 input = context.ReadValue<Vector2>();
         
         _heldMovementInput = Math.Sign(input.x);
+    }
+
+    public void HandleJumpInput(InputAction.CallbackContext context)
+    {
+        Debug.Log("I'm like hey what's up hello");
+        if (groundCheck.CheckGrounded() && context.performed)
+        {
+            entityMovement.Jump();
+        }
     }
 }
