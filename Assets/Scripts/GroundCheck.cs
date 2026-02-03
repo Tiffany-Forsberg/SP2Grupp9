2ff152f974071f94e7966036a6ef6412b1e7614f
a24phizi <a24phizi@student.his.se>
2026-02-03 12:08:06 +0100

    Update ground check
    This should work temporarily until we figure out raycasting the ground instead :)
    
    Co-Authored-By: Tiffany Forsberg <142976914+Tiffany-Forsberg@users.noreply.github.com>

--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +10,17 @@ public class GroundCheck : MonoBehaviour
     private Vector2 _groundDirection = Vector2.down;
     
     [SerializeField] private LayerMask groundLayer;
+
+    [SerializeField, Range(0f, 1f)] private float groundNormalTolerance;
+
+    private bool _onGround => _groundObjects.Count > 0;
+
+    private List<GameObject> _groundObjects;
+
+    private void Awake()
+    {
+        _groundObjects = new List<GameObject>();
+    }
 
     void FixedUpdate()
     {
@@ -24,15 +36,47 @@ public class GroundCheck : MonoBehaviour
     }    
     public bool CheckGrounded()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(transform.position, groundCastSize, 0, _groundDirection, groundCastSize.y, groundLayer);
-        return hit;
+        return _onGround;
+
+        /*RaycastHit2D hit = Physics2D.BoxCast(transform.position, groundCastSize, 0, _groundDirection, groundCastSize.y, groundLayer);
+        return hit;*/
     }
     
-    //Draw the BoxCast as a gizmo to show where it currently is testing. Click the Gizmos button to see this
+    // Draw the BoxCast as a gizmo to show where it currently is testing. Click the Gizmos button to see this
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
 
         Gizmos.DrawWireCube(transform.position, groundCastSize);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(((1<<other.gameObject.layer) & groundLayer) != 0)
+        {
+            Vector2 averageGroundNormal = new Vector2();
+
+            for (int i = 0; i < other.contactCount; i++)
+            {
+                averageGroundNormal += other.contacts[i].normal;
+            }
+        
+            averageGroundNormal /= other.contactCount;
+        
+            averageGroundNormal.Normalize();
+
+            if (averageGroundNormal.y >= groundNormalTolerance)
+            {
+                _groundObjects.Add(other.gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (_groundObjects.Contains(other.gameObject))
+        {
+            _groundObjects.Remove(other.gameObject);
+        }
     }
 }
