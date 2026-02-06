ba8085ee5d1ab04f81dbf4893a5d5ba531835a2d
Tiffany Forsberg <tiff.forsberg@gmail.com>
2026-02-06 11:38:25 +0100

    Add Cinemachine camera

---
+++ Assets/Scripts/DisplayConfounds.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DisplayConfounds : MonoBehaviour
+{
+    [SerializeField] private BoxCollider2D confoundCollider;
+
+    private void OnValidate()
+    {
+        if (!confoundCollider) 
+        {
+            Debug.LogWarning("Display Confounds is missing a reference to a Box Collider 2D :(", this);
+        }
+    }
+    
+    private void OnDrawGizmos()
+    {
+        if (!confoundCollider) return;
+        
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(confoundCollider.transform.position, confoundCollider.size);
+    }
+}
