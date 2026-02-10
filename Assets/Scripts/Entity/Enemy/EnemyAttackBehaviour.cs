122bab0eb2d5d5148f9cb2a151d91d401a202bc5
Tiffany Forsberg <tiff.forsberg@gmail.com>
2026-02-10 15:14:17 +0100

    Add groundwork for enemy behaviours
    Co-Authored-By: a24phizi <187495987+a24phizi@users.noreply.github.com>

---
+++ Assets/Scripts/Entity/Enemy/EnemyAttackBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackBehaviour : MonoBehaviour
+{
+    [SerializeField] private EntityStats entityStats;
+    [SerializeField] private LayerMask hostileLayer;
+    [SerializeField] private List<AbilityExecutor> executors;
+    [SerializeField] private AttackBehaviour attackPrefab;
+
+    [SerializeReference]
+    private EnemyAttackPattern pattern;
+
+    private void FixedUpdate()
+    {
+        pattern.Execute(entityStats, hostileLayer, executors, attackPrefab);
+    }
+}
