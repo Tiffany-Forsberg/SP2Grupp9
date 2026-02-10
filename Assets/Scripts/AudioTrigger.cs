9cc34c65636d8a9f67f5eb2e37af5bff0e40794b
Balgruuff <a24axeas@student.his.se>
2026-02-10 13:20:25 +0100

    AudioTrigger script
    Also edited AudioManager script

---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    public enum Action
+    {
+        Play,
+        Stop
+    }
+    [Serializable]
+    public struct AudioSettings
+    {
+        public Location location;
+        public Location action;
+    }
+
+    public AudioSettings[] triggerEnterSettings;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (AudioSettings i in triggerEnterSettings)
+            {
+                switch (i.action)
+                {
+                    case Action.Play:
+                        AudioManager.Instance.PlayAudio(i.location);
+                        break;
+                    case Action.Stop:
+                        AudioManager.Instance.StopAudio(i.location);
+                        break;
+                }
+            }
+        }
+    }
+}
