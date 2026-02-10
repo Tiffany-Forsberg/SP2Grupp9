c3afef340f537a787ac3b0fa0f9827edc1fba517
Gukara02 <a24guser@student.his.se>
2026-02-10 14:22:40 +0100

    started on Selene aduio scripts

--- Assets/Scripts/Audio Scripts/SeleneAudio.cs
+++ Assets/Scripts/Audio Scripts/SeleneAudio.cs
@@ -4,7 +4,51 @@ using FMOD.Studio;
 
 [CreateAssetMenu(fileName = "CharacterAudio", menuName = "Scriptable Objects/CharacterAudio")]
 
-public class SeleneAudio : MonoBehaviour
+public class SeleneAudio : ScriptableObject
 {
-   
+    [SerializeField]
+    private EventReference footstepEventRef, clawAttackEventRef, jumpEventRef, landEventRef, damageEventRef;
+
+    private EventInstance footstepEventInst, clawAttackEventInst, jumpEventInst, landEventInst, damageEventInst;
+    
+   /* public void FootstepEventPlay(string surfaceTag, GameObject obj)
+    {
+        if (footstepEvent.IsNull)
+        {
+            Debug.LogWarning("Event not found: footstepEvent");
+        }
+        else
+        {
+            footstepEventInst = RuntimeManager.CreateInstance(footstepEventRef);
+            RuntimeManager.AttachInstanceToGameObject(footstepEventInst, obj, obj.GetComponent<Rigidbody>());
+            
+            // A switch statement that compares the surfaceTag and its content, and sets the Surface paramteter wth a unique value based on this
+            switch (surfaceTag)
+            {
+                case "Stone":
+                    footstepEventInst.setParameterByName("SurfaceTag", 0f);
+                    break;
+                case "Dirt":
+                    footstepEventInst.setParameterByName("SurfaceTag", 1f);
+                    break;
+                case "Metal":
+                    footstepEventInst.setParameterByName("SurfaceTag", 2f);
+                    break;
+            }
+            
+            footstepEventInst.start();
+            footstepEventInst.release();
+        }
+    }
+
+    public void clawAttackPlay(GameObject obj)
+    {
+        clawAttackEventInst = RuntimeManager.CreateInstance(clawAttackEventRef);
+
+        RuntimeManager.AttachInstanceToGameObject(clawAttackEventInst, obj, obj.GetComponent<Rigidbody>());
+
+        clawAttackEventInst.start();
+
+        clawAttackEventInst.release();
+    }*/
 }
