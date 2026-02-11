41456f29f21b0bd54ba00937bc305fd1fb04a376
Tiffany Forsberg <tiff.forsberg@gmail.com>
2026-02-11 16:08:36 +0100

    Add VectorEvent class
    Co-Authored-By: a24phizi <187495987+a24phizi@users.noreply.github.com>

---
+++ Assets/Scripts/Entity/VectorEvent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+public class VectorEvent : MonoBehaviour
+{
+    public Action<Vector2> OnVectorEvent;
+    
+    public void InvokeOnVectorEvent(Vector2 vector) => OnVectorEvent?.Invoke(vector);
+}
