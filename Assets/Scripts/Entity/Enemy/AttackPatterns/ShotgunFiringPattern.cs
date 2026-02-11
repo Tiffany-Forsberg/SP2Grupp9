ffd6b2ffb29d709abd73cd5e9feafcc9d32f1927
a24phizi <a24phizi@student.his.se>
2026-02-11 17:12:17 +0100

    Flying Squirrel enemy now fires and moves properly
    - Add a bullet to it in the future

--- Assets/Scripts/Entity/Enemy/AttackPatterns/ShotgunFiringPattern.cs
+++ Assets/Scripts/Entity/Enemy/AttackPatterns/ShotgunFiringPattern.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
 using ImprovedTimers;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ShotgunFiringPattern : EnemyAttackPattern
 {
-    [SerializeField] private float maxFiringDistance;
+    [SerializeField] private VectorEvent firingEvent;
     
     [Tooltip("The normalized angle from the player direction that the two extra bullets should be fired (0 = 0 degrees, 1 = pi/180 degrees)")]
     [SerializeField, Range(0f, 1f)] private float firingArch;
@@ -12,27 +13,70 @@ public class ShotgunFiringPattern : EnemyAttackPattern
     [Tooltip("The randomization applied to the firing arch value, high values may result in the extra bullets being fired in very erratic directions")]
     [SerializeField, Range(0f, 1f)] private float archRandomization;
 
-    [Tooltip("The fire rate of this enemy")]
+    /*[Tooltip("The fire rate of this enemy")]
     [SerializeField] private float fireRate;
-    private CountdownTimer firingCooldown;
-    
+    private CountdownTimer firingCooldown;*/
+
+    private bool fire = false;
+    private Vector2 firingDirection;
+
+    public override void Setup()
+    {
+        firingEvent.OnVectorEvent += AllowFiring;
+    }
+
     public override void Execute(EntityStats stats, LayerMask hostileLayer, List<AbilityExecutor> executors, AttackBehaviour attackPrefab)
     {
-        if (firingCooldown is null)
+        if (fire)
         {
-            firingCooldown = new CountdownTimer(fireRate);
-            firingCooldown.Reset();
+            fire = false;
+            
+            FireInDirection(firingDirection, stats, hostileLayer, executors, attackPrefab);
+            
+            /*if (firingCooldown is null)
+            {
+                firingCooldown = new CountdownTimer(fireRate);
+                firingCooldown.Reset();
+                firingCooldown.Start();
+            }
+            
+            if (!firingCooldown.IsFinished)
+            {
+                return;
+            }
+            
+            fire = false;
+            
+            
+            
+            firingCooldown.Reset();*/
         }
+    }
+
+    private void FireInDirection(Vector2 direction, EntityStats stats, LayerMask hostileLayer, List<AbilityExecutor> executors, AttackBehaviour attackPrefab)
+    {
+        AttackBehaviour bullet1 = UnityEngine.Object.Instantiate(attackPrefab, stats.transform.position, Quaternion.identity);
+        attackPrefab.Setup(executors, hostileLayer, direction);
+
+        Vector2 localX = direction.normalized;
+        Vector2 localY = new Vector2(direction.y, -direction.x).normalized;
+
+        float extraAngle1 = Random.Range(-archRandomization, archRandomization);
+        Vector2 exBullet1Direction = localX * Mathf.Cos(firingArch + extraAngle1) + localY * Mathf.Sin(firingArch + extraAngle1);
         
-        if (!firingCooldown.IsFinished)
-        {
-            return;
-        }
+        AttackBehaviour exBullet1 = UnityEngine.Object.Instantiate(attackPrefab, stats.transform.position, Quaternion.identity);
+        exBullet1.Setup(executors, hostileLayer, exBullet1Direction);
         
-        // Fire
+        float extraAngle2 = Random.Range(-archRandomization, archRandomization);
+        Vector2 exBullet2Direction = localX * Mathf.Cos(firingArch + extraAngle2) - localY * Mathf.Sin(firingArch + extraAngle2);
         
-        
-        
-        firingCooldown.Reset();
+        AttackBehaviour exBullet2 = UnityEngine.Object.Instantiate(attackPrefab, stats.transform.position, Quaternion.identity);
+        exBullet2.Setup(executors, hostileLayer, exBullet2Direction);
+    }
+
+    public void AllowFiring(Vector2 direction)
+    {
+        fire = true;
+        firingDirection = direction;
     }
 }
--- Assets/Scripts/Entity/Enemy/Movement/HelicopterMovementPattern.cs
+++ Assets/Scripts/Entity/Enemy/Movement/HelicopterMovementPattern.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
+using ImprovedTimers;
+using UnityEngine.Events;
 
+/// <summary>
+/// Moves in a helicopter-esque pattern that stays a certain distance within and away from the player.
+/// Enemies with this movement pattern should not be affected by gravity and will fly
+/// </summary>
 public class HelicopterMovementPattern : EnemyMovementPattern
 {
+    [SerializeField] private VectorEvent firingEvent;
+    
     private GameObject _playerObject;
     // THIS IS A TEMP SOLUTION, PLEASE FIND A BETTER WAY AND REMOVE THIS ASAP
     [Tooltip("The name of the player object, to be removed in favor of a better way asap")]
     [SerializeField] private string playerName;
 
-    [SerializeField] private float preferredRangeToPlayer;
+    [Tooltip("This is not a vector, but a range that this enemy prefers to stay within relative to the player, X = min distance and Y = max distance")]
+    [SerializeField] private Vector2 preferredRangeToPlayer;
 
     [SerializeField] private float speed;
+
+    [SerializeField] private float moveCycleCooldown;
+    private CountdownTimer moveCycleTimer;
 
     public override void Setup()
     {
@@ -17,6 +29,10 @@ public class HelicopterMovementPattern : EnemyMovementPattern
         {
             _playerObject = GameObject.Find(playerName);
         }
+        
+        moveCycleTimer = new CountdownTimer(moveCycleCooldown);
+        moveCycleTimer.Reset();
+        moveCycleTimer.Start();
     }
 
     public override void Execute(EntityMovement movement, LayerMask hostileLayers, GroundCheck groundCheck)
@@ -26,9 +42,25 @@ public class HelicopterMovementPattern : EnemyMovementPattern
         Vector2 positionDelta = _playerObject.transform.position - movement.transform.position;
         Vector2 deltaDirection = positionDelta.normalized; // Towards player
         
-        if (positionDelta.magnitude < preferredRangeToPlayer)
+        if (positionDelta.magnitude < preferredRangeToPlayer.x)
         {
             movement.ApplyDirectVelocity(-deltaDirection * speed);
+        }
+        else if (positionDelta.magnitude > preferredRangeToPlayer.y)
+        {
+            movement.ApplyDirectVelocity(deltaDirection * speed);
+        }
+        else
+        {
+            if (moveCycleTimer.IsFinished)
+            {
+                Vector2 randomMovement = Random.insideUnitCircle * speed;
+                movement.ApplyDirectVelocity(randomMovement);
+                moveCycleTimer.Reset();
+                moveCycleTimer.Start();
+                
+                firingEvent.OnVectorEvent?.Invoke(deltaDirection);
+            }
         }
     }
 }
