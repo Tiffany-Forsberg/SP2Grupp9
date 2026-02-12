f372cd860c16b9eec6335a890f8c17579c029732
Tiffany Forsberg <tiff.forsberg@gmail.com>
2026-02-12 12:08:35 +0100

    Continue work on SlashGrunt movement
    Does not work at all, please send help

--- Assets/Scripts/Entity/Enemy/Movement/StepMovementPattern.cs
+++ Assets/Scripts/Entity/Enemy/Movement/StepMovementPattern.cs
@@ -9,6 +9,8 @@ public class StepMovementPattern : EnemyMovementPattern
     [Min(0.0f)] [SerializeField] private float _tinyStepDuration;
     [Min(0.0f)] [SerializeField] private float _bigStepSpeed;
     [Min(0.0f)] [SerializeField] private float _bigStepDuration;
+
+    [SerializeField] private Collider2D collider;
     
     [Tooltip("Handles the characters movement direction")]
     public bool MovingRight = true;
@@ -17,6 +19,10 @@ public class StepMovementPattern : EnemyMovementPattern
     
     private bool _isSlowStepping;
     private float _timer;
+
+    [HideInInspector] public bool ShouldUseTargetPosition;
+    
+    private Vector2 _targetPosition;
     
     public override void Execute(EntityMovement movement, LayerMask hostileLayers, GroundCheck groundCheck)
     {
@@ -27,18 +33,30 @@ public class StepMovementPattern : EnemyMovementPattern
         }
     }
 
+    public void SetTargetPosition(Vector2 targetPosition)
+    {
+        _targetPosition = targetPosition;
+    }
+
     private void HandleStartStepLogic(EntityMovement movement)
     {
         _isSlowStepping = !_isSlowStepping;
         if (_isSlowStepping)
         {
-            movement.HandleStep(_tinyStepSpeed * Direction, () => _isSlowStepping);
+            movement.HandleStep(_tinyStepSpeed * Direction, () => _isSlowStepping && !HandleOverlap());
             _timer = _tinyStepDuration;
         }
         else
         {
-            movement.HandleStep(_bigStepSpeed * Direction, () => !_isSlowStepping);
+            movement.HandleStep(_bigStepSpeed * Direction, () => !_isSlowStepping && !HandleOverlap());
             _timer = _bigStepDuration;
         }
+    }
+
+    private bool HandleOverlap()
+    {
+        if (!ShouldUseTargetPosition) return false;
+        if (collider.OverlapPoint(_targetPosition)) return true;
+        return false;
     }
 }
