c074a617be53fcfb43deaa6abeff2a2b681c15a7
Gukara02 <a24guser@student.his.se>
2026-02-12 15:44:02 +0100

    Script fixed so footsteps can be played

--- Assets/Scripts/Entity/Player/PlayerAudioController.cs
+++ Assets/Scripts/Entity/Player/PlayerAudioController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerAudioController : MonoBehaviour
 {
     public SeleneAudio SeleneAudio;
-    public GameObject footstepSource;
+    public GameObject footstepSource, jumpSource, landingSource;
     
     public void FootstepPlay()
     {
@@ -19,19 +19,57 @@ public class PlayerAudioController : MonoBehaviour
             
             Debug.Log("Yay!");
         }
-        
-        
+    }
 
-        /*if (Physics.Raycast(footstepSource.transform.position, Vector3.down, out hit,1f))
+    public void ClawAttackPlay()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(footstepSource.transform.position, Vector2.down, 1f,
+            LayerMask.GetMask("Ground"));
+        if (hit)
         {
             Debug.Log("FootstepPlay on: " + hit.collider.gameObject.tag);
             SeleneAudio.FootstepEventPlay(hit.collider.tag, footstepSource);
+            
+            Debug.Log("rawr!");
         }
-        else
-        {
-            Physics.Raycast(footstepSource.transform.position, Vector3.down, out hit, 1f);
-            Debug.Log("No sound for you..." + hit);
-        }*/
     }
-    
+
+    public void JumpEventPlay()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(jumpSource.transform.position, Vector2.down, 1f,
+            LayerMask.GetMask("Ground"));
+        if (hit)
+        {
+            Debug.Log("FootstepPlay on: " + hit.collider.gameObject.tag);
+            SeleneAudio.JumpEventPlay(hit.collider.tag, jumpSource);
+            
+            Debug.Log("jumpwoman!");
+        }
+    }
+
+    public void LandingPlay()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(landingSource.transform.position, Vector2.down, 1f,
+            LayerMask.GetMask("Ground"));
+        if (hit)
+        {
+            Debug.Log("FootstepPlay on: " + hit.collider.gameObject.tag);
+            SeleneAudio.LandEventPlay(hit.collider.tag, landingSource);
+            
+            Debug.Log("land!");
+        }
+    }
+
+    public void SeleneDamageEventPlay()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(footstepSource.transform.position, Vector2.down, 1f,
+            LayerMask.GetMask("Ground"));
+        if (hit)
+        {
+            Debug.Log("FootstepPlay on: " + hit.collider.gameObject.tag);
+            SeleneAudio.FootstepEventPlay(hit.collider.tag, footstepSource);
+            
+            Debug.Log("oof!");
+        }
+    }
 }
