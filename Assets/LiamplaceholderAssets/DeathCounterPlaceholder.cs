902627b4c08a738d86a0fcfc7846399a3108c3e0
liammcgarvie <mcgarvieliam05@gmail.com>
2026-02-12 10:58:39 +0100

    Added new script for counting Deaths

---
+++ Assets/LiamplaceholderAssets/DeathCounterPlaceholder.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using SystemCollections;
+using SystemCollections.Generic;
+
+public class EnemyDeathCounterControllerPlaceholder : MonoBehaviour
+{
+    int maxD = 0;
+    int curD = 0;
+
+    public void Count() 
+    {
+        curD++;
+
+        if(curD > MaxD)
+        {
+
+        }
+    }
+}
+
