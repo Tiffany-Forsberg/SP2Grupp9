c402854fb4a3b844c87662ca7e48b004a26c8c61
Gukara02 <a24guser@student.his.se>
2026-02-12 15:15:59 +0100

    Script fixed so footsteps can be played

--- Assets/Scripts/Entity/Player/PlayerAudioController.cs
+++ Assets/Scripts/Entity/Player/PlayerAudioController.cs
@@ -2,22 +2,36 @@ using UnityEngine;
 
 public class PlayerAudioController : MonoBehaviour
 {
-    public SeleneAudio  SeleneAudio;
+    public SeleneAudio SeleneAudio;
     public GameObject footstepSource;
     
     public void FootstepPlay()
     {
-        Debug.DrawRay(footstepSource.transform.position, Vector3.down*1f, Color.red, duration:1f);
-        RaycastHit hit;
-        if (Physics.Raycast(footstepSource.transform.position, Vector3.down, out hit,1f))
+        //Debug.DrawRay(footstepSource.transform.position, Vector3.down*1f, Color.red, duration:1f);
+        //RaycastHit hit;
+
+        RaycastHit2D hit = Physics2D.Raycast(footstepSource.transform.position, Vector2.down, 1f,
+            LayerMask.GetMask("Ground"));
+        if (hit)
+        {
+            Debug.Log("FootstepPlay on: " + hit.collider.gameObject.tag);
+            SeleneAudio.FootstepEventPlay(hit.collider.tag, footstepSource);
+            
+            Debug.Log("Yay!");
+        }
+        
+        
+
+        /*if (Physics.Raycast(footstepSource.transform.position, Vector3.down, out hit,1f))
         {
             Debug.Log("FootstepPlay on: " + hit.collider.gameObject.tag);
             SeleneAudio.FootstepEventPlay(hit.collider.tag, footstepSource);
         }
         else
         {
-            Debug.Log("No hit...");
-        }
+            Physics.Raycast(footstepSource.transform.position, Vector3.down, out hit, 1f);
+            Debug.Log("No sound for you..." + hit);
+        }*/
     }
     
 }
